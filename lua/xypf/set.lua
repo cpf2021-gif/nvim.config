e070f29a31996e2927bbd93a18da2d0b3702e673
cpf2021-gif <2992247892@qq.com>
2023-10-07 15:13:46 +0800

    feat: update set.lua

--- lua/xypf/set.lua
+++ lua/xypf/set.lua
@@ -8,6 +8,11 @@ vim.opt.expandtab = true
 
 vim.opt.smartindent = true
 
+vim.opt.swapfile = false
+vim.opt.backup = false
+vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+vim.opt.undofile = true
+
 vim.opt.wrap = false
 
 vim.opt.termguicolors = true
